07856fbee6487c62ab93a9bc81bc01415114b08c
GregOwen <greowen@gmail.com>
2017-12-22 00:02:53 -0800

    Test that we clean up tracker

--- tests/integration.rs
+++ tests/integration.rs
@@ -33,20 +33,25 @@ fn end_to_end_test() {
 
     // verify binary had desired effect
     verify_new_messages(&temp_dir_path);
-
-    // clean up
 }
 
 fn setup_temp_dir(dir_path: &Path) {
-    setup_git_history(&dir_path);
-    gitfun::write_str_to_file(
-        NEW_COMMIT_MESSAGE_CONTENTS,
-        &dir_path.join(NEW_COMMIT_MESSAGE_FILE).to_string_lossy());
+    gitfun::exec_command("git init", dir_path.to_str());
+    setup_message_file(&dir_path);
+    setup_commit_files(&dir_path);
 }
 
-fn setup_git_history(dir_path: &Path) {
-    gitfun::exec_command("git init", dir_path.to_str());
-    for i in 0..NUM_COMMITS {
+fn setup_message_file(dir_path: &Path) {
+    let file_path = &dir_path.join(NEW_COMMIT_MESSAGE_FILE);
+    let file_name = file_path.to_string_lossy();
+    gitfun::write_str_to_file(NEW_COMMIT_MESSAGE_CONTENTS, &file_name);
+    gitfun::exec_command(
+        &format!("git add {} && git commit -m 'message file'", file_name),
+        dir_path.to_str());
+}
+
+fn setup_commit_files(dir_path: &Path) {
+    for i in 1..NUM_COMMITS {
         add_file_and_commit(dir_path, i);
     }
 }
@@ -65,7 +70,7 @@ fn verify_setup(dir_path: &Path) {
     let dir_str = dir_path.to_str();
     let ls_res = gitfun::exec_command("ls", dir_str).stdout;
     let files = String::from_utf8_lossy(&ls_res);
-    assert_eq!(files.trim_right().split("\n").collect::<Vec<&str>>().len(), NUM_COMMITS + 1);
+    assert_eq!(files.trim_right().split("\n").collect::<Vec<&str>>().len(), NUM_COMMITS);
 
     let git_res = gitfun::exec_command("git log --pretty=oneline", dir_str).stdout;
     let commits = String::from_utf8_lossy(&git_res);
@@ -89,7 +94,32 @@ fn get_executable_path() -> String {
 
 fn verify_new_messages(dir_path: &Path) {
     let dir_str = dir_path.to_str();
+
+    // Correct git commit messages
     let git_res = gitfun::exec_command("git log --pretty=format:%s", dir_str).stdout;
     let commits = String::from_utf8_lossy(&git_res);
     assert_eq!(commits.trim_right(), NEW_COMMIT_MESSAGE_CONTENTS);
+
+    // No new files
+    let ls_res = gitfun::exec_command("ls -a", dir_str).stdout;
+    let files = String::from_utf8_lossy(&ls_res);
+
+    // . .. .git
+    let desired_num_files = NUM_COMMITS + 3;
+    let observed_num_files = files.trim_right().split("\n").collect::<Vec<&str>>().len();
+    assert_eq!(
+        observed_num_files,
+        desired_num_files,
+        "Expected {} files but found {}.
+$ ls -a
+{}",
+        desired_num_files,
+        observed_num_files,
+        files
+    );
+
+    // No changes to existing files
+    let status_res = gitfun::exec_command("git status -s", dir_str).stdout;
+    let status = String::from_utf8_lossy(&status_res);
+    assert_eq!(status.trim_right(), "");
 }
